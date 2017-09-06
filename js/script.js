aa6d818270fcd4658b3f10f33a86b66d2e80d328
Lissa1990 <alisa.andreevna@mail.ru>
2017-09-06 11:31:40 +0300

    Правки

--- js/script.js
+++ js/script.js
@@ -6,7 +6,8 @@ var dateoff = document.querySelector("[name=date-off]");
 var grownups = document.querySelector("[name=grownups]");
 var children = document.querySelector("[name=children]");
 
-link.addEventListener("click", function (evt) {evt.preventDefault(); modal.classList.toggle("form-close");});
+link.addEventListener("click", function (evt) {evt.preventDefault(); modal.classList.toggle("form-close"); 
+	if (!modal.classList.contains("form-close")) {modal.classList.add("form-open")}});
 form.addEventListener("submit", function (evt) {evt.preventDefault(); 
 	if (!dateon.value  || !dateoff.value || !grownups.value || !children.value) {evt.preventDefault(); modal.classList.add("form-error")} 
 	else {modal.classList.remove("form-error")}});
